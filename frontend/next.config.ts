7f24ac0414a403b04f12db2596ef9469974cf228
Tucnak28 <Tucnak28@email.cz>
2025-05-18 15:47:58 +0200

    Removed allowed origins

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-
-  allowedDevOrigins: [
-    "http://192.168.0.172:3000",
-  ],
 };
 
 export default nextConfig;
